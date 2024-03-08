77423dfe74f7d9beacad730f0d2fa94ba4ad96cb
szalaibator <szalai.arpad.bator@students.jedlik.eu>
2024-03-08 11:34:08 +0100

    validator middleware updated

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -1,4 +1,4 @@
-// import { plainToInstance } from "class-transformer";
+import { plainToInstance } from "class-transformer";
 import { validate, ValidationError } from "class-validator";
 import express from "express";
 
@@ -8,18 +8,23 @@ import HttpException from "../exceptions/HttpException";
 export default function validationMiddleware(type: any, skipMissingProp = false): express.RequestHandler {
     return (req, res, next) => {
         // forbidUnknownValues: false
-        const validatorObject = Object.assign(new type(), req.body);
-        validate(validatorObject, { skipMissingProperties: skipMissingProp }).then((errors: ValidationError[]) => {
-            if (errors.length > 0) {
+        // const validatorObject = Object.assign(new type(), req.body);
+        validate(plainToInstance(type, req.body), { skipMissingProperties: skipMissingProp }).then((errors: ValidationError[]) => {
+            if (errors!.length > 0) {
                 // Break down, if validate nested object in latest version of class-validator
                 // const message = errors.map((error: ValidationError) => Object.values(error.constraints)).join(", ");
-                let message = "";
-                if (errors[0].constraints) {
-                    message = errors.map((error: ValidationError) => Object.values(error.constraints)).join(", ");
-                } else {
-                    message = "Error on check DTO-s ";
-                    message += errors.map((error: ValidationError) => Object.values(error.children[0].constraints)).join(", ");
-                    // message: " an unknown value was passed to the validate function"
+                let message = "DTO error:";
+                for (let i = 0; i < errors.length; i++) {
+                    if (errors[i].constraints) {
+                        message += `${Object.values(errors[i].constraints)}, `;
+                    }
+                    if (errors[i].children!.length > 0) {
+                        for (let j = 0; j < errors[i].children.length; j++) {
+                            if (errors[i].children[j].constraints) {
+                                message += `${errors[i].property}.${Object.values(errors[i].children[j].constraints)}, `;
+                            }
+                        }
+                    }
                 }
                 next(new HttpException(400, message));
             } else {
